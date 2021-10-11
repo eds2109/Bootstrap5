78d4ce4779afbb27b3fe0c111515e28020caa91d
eds2109 <warlock609@gmail.com>
2021-10-11 14:00:31 +0300

    add style header-top

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(function() {
 
+  $('.open-search').click(function(e) {
+    e.preventDefault();
+    $('#search').addClass('active');
+  });
+
+  $('.close-search').click(function() {
+    $('#search').removeClass('active')
+  })
 
 });
