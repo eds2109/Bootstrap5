cfeeee48ccd19bf53eddbe6bac269f2593a6db4d
eds2109 <warlock609@gmail.com>
2021-10-16 15:44:39 +0300

    add button top

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 $(function() {
 
-  $('.open-search').click(function(e) {
-    e.preventDefault();
-    $('#search').addClass('active');
-  });
+  /* Кнопка иконка поска в верхней панели */
+  $('.open-search').click(function (e) {
+    e.preventDefault()
+    $('#search').addClass('active')
+  })
 
-  $('.close-search').click(function() {
+  $('.close-search').click(function () {
     $('#search').removeClass('active')
   })
 
+  /* Кнопка вверх */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('#top').fadeIn()
+    } else {
+      $('#top').fadeOut();
+    }
+  });
+
+  /* Обработчик срола вверх */
+  $('#top').click(function() {
+    $('body, html').animate({scrollTop: 0}, 300);
+  });
 });
